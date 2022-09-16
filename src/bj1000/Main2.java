ae13797c1f6ace25099356c74e41891ac25d169e
cks <cksun2598@gmail.com>
2022-09-16 15:58:58 +0900

    f

---
+++ src/bj1000/Main2.java
@@ -0,0 +1,26 @@
+package bj1000;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main2 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String str = br.readLine();
+		
+		// StringTokenizer( "문자열" , 구분자 ); -> 구분자 단위로 문자열을 토큰으로 자름
+		StringTokenizer st = new StringTokenizer(str, " "); 
+		
+		// nextToken(); -> 문자열 반환
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+	
+		System.out.println(a+b);
+	}
+
+}
--- src/bj1001/Main.java
+++ src/bj1001/Main.java
@@ -1,17 +1,17 @@
-package bj1000;
+package bj1001;
 
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-
+		
 		Scanner sc = new Scanner(System.in);
 
 		int A = sc.nextInt();
 		int B = sc.nextInt();
 		
-		System.out.println(A+B);
+		System.out.println(A-B);
 		
 		sc.close();
 	}
