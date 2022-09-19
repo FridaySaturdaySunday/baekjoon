14d5f309a36b960476c0cae2aa3d016943f5208b
cks <cksun2598@gmail.com>
2022-09-19 21:52:07 +0900

    e

--- src/bj2480/Main.java
+++ src/bj2480/Main.java
@@ -13,7 +13,39 @@ public class Main {
 		
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
 		
+		int prize;
+		
+		if(a==b && b==c) {
+			prize = 1000 + (a * 1000);
+		} else if (a==b) {
+			prize = 1000 + (a * 100);
+		} else if (b==c) {
+			prize = 1000 + (b * 100);
+		} else if (a==c) {
+			prize = 1000 + (a * 100);
+		} else {
+			int max;
+			if(a>b) {
+				if(c>a) {
+					max = c;
+				} else {
+					max = a;
+				}
+			} else { // a<b
+				if(c>b) {
+					max = c;
+				} else {
+					max = b;
+				}
+			}
+			prize = max * 100;
+		}
+		
+		System.out.println(prize);
 	}
 
 }
