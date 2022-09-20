b2ced473058795cd27f29e620be9532669bb45a6
cks <cksun2598@gmail.com>
2022-09-20 19:20:53 +0900

    f

--- src/bj25304/Main.java
+++ src/bj25304/Main.java
@@ -1,9 +1,14 @@
 package bj25304;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Main {
 
 	public static void main(String[] args) {
 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
 		
 	}
 
