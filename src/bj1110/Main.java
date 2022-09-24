90951051ff44d320f3f9169f8872a084496dbc2b
cks <cksun2598@gmail.com>
2022-09-24 23:29:32 +0900

    g

--- src/bj1110/Main.java
+++ src/bj1110/Main.java
@@ -1,10 +1,32 @@
 package bj1110;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
+		int n = Integer.parseInt(br.readLine());
+		
+		int count = 0;
+		int copy = n;
+		
+		while(true) {
+			
+			n = ((n%10)*10) + (((n/10) + (n%10)) % 10);
+			
+			count++;
+			
+			if(copy == n) {
+				break;
+			}
+		}
+		System.out.println(count);
+				
 	}
 
 }
