af88b15bf885817bfecbcbc3d05c31662e2ef329
cks <cksun2598@gmail.com>
2022-10-01 00:05:21 +0900

    j

---
+++ src/bj4673/Main.java
@@ -0,0 +1,19 @@
+package bj4673;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		for(int i=1; i<=10000; i++) {
+			int n = d(i);
+		}
+	}
+
+	public static int d (int number) {
+		int d = 0;
+
+		
+		return d;
+	}
+
+	
+}
