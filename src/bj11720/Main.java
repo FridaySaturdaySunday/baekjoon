a10c799f30cc0003f32e727bd35e5c64dbaba227
cks <cksun2598@gmail.com>
2022-10-05 21:07:39 +0900

    ㅎㅎ

--- src/bj11720/Main.java
+++ src/bj11720/Main.java
@@ -17,7 +17,7 @@ public class Main {
 		// getBytes() : 입력 문자열을 byte 단위의 '배열'로 반환시켜주는 메소드
 		for(byte a : br.readLine().getBytes()) {
 			sum += a - '0'; 
-			// getBytes()로 변환하는 방법은 UTF-16 (유니코드) 방식을 따르므로 반드시 '0' 또는 48 을 빼주어야
+			// getBytes()로 int형으로 변환하는 방법은 UTF-16 (유니코드) 방식을 따르므로 반드시 '0' 또는 48 을 빼주어야
 		}
 		
 		System.out.println(sum);
---
+++ src/bj2675/Main.java
@@ -0,0 +1,41 @@
+package bj2675;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int t = Integer.parseInt(br.readLine());
+		
+		for(int i=0; i<t; i++) {
+			
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			
+			int r = Integer.parseInt(st.nextToken());
+			
+			String str = st.nextToken();
+			
+			// getBytes() : 입력 문자열을 byte 단위의 '배열'로 반환시켜주는 메소드
+			for(byte a : str.getBytes()) {
+				
+				for(int j=0; j<r; j++) {
+					sb.append((char)a);
+				} // getBytes()는 byte형이므로 (char)로 강제형변환 하면됨.
+				
+			}
+			
+			sb.append("\n");
+			
+		}
+		
+		System.out.println(sb);
+	}
+
+}
