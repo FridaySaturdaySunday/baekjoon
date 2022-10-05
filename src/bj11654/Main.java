d1856c87bc773a2e41709aa08dda7776591cf54c
cks <cksun2598@gmail.com>
2022-10-05 19:15:22 +0900

    j

--- src/bj11654/Main.java
+++ src/bj11654/Main.java
@@ -1,9 +1,17 @@
 package bj11654;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+// 알파벳 소문자, 대문자, 숫자 0-9중 하나가 주어졌을 때, 주어진 글자의 아스키 코드값을 출력하는 프로그램을 작성하시오.
+	
 	public static void main(String[] args) {
 
+		Scanner sc = new Scanner(System.in);
+		
+		int ch = sc.next().charAt(0); // 어떤 타입이 입력되든 char형으로 변환 후 int형 변수에 저장
+		
+		System.out.println(ch);
 		
 	}
 
