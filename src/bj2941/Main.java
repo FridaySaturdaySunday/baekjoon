0c45f54767afd8606386f531a85e046def12c6c7
cks <cksun2598@gmail.com>
2022-10-10 23:50:23 +0900

    h

--- src/bj2941/Main.java
+++ src/bj2941/Main.java
@@ -1,9 +1,56 @@
 package bj2941;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String str = br.readLine();
+		
+		int count = 0;
+		
+		for(int i=0; i<str.length(); i++) {
+			
+			char ch = str.charAt(i);
+			
+			if(ch == 'c' && i < str.length() - 1) { // StringIndexOutOfBoundsException 발생 방지
+				if(str.charAt(i+1) == '=' || str.charAt(i+1) == '-') {
+					i++;
+				}
+			} else if(ch == 'd' && i < str.length() - 1) {
+				if(str.charAt(i+1) == 'z' && i < str.length() - 2) {
+					if(str.charAt(i+2) == '=') {
+						i+=2;
+					}
+				} else if (str.charAt(i+1) == '-') {
+					i++;
+				}
+			} else if(ch == 'l' && i < str.length() - 1) {
+				if(str.charAt(i+1) == 'j') {
+					i++;
+				}
+			} else if(ch == 'n' && i < str.length() - 1) {
+				if(str.charAt(i+1) == 'j') {
+					i++;
+				}
+			} else if(ch == 's' && i < str.length() - 1) {
+				if(str.charAt(i+1) == '=') {
+					i++;
+				}
+			} else if(ch == 'z' && i < str.length() - 1) {
+				if(str.charAt(i+1) == '=') {
+					i++;
+				}
+			}
+			count++;
+		}
+		
+		System.out.println(count);
 		
 	}
 
