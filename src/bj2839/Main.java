2f639e61d3a0deb97f46ab1879fa551192321769
cks <cksun2598@gmail.com>
2022-10-18 21:04:18 +0900

    G

--- src/bj2839/Main.java
+++ src/bj2839/Main.java
@@ -1,10 +1,36 @@
 package bj2839;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws NumberFormatException, IOException {
 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+//		n 이 4 또는 7 인 경우
+//		n 이 5의 배수인경우 ( 5로 나눈 나머지가 0 인 경우 )
+//		n 이 5의 배수 + 1 또는 5의 배수 + 3 인 경우 ( 5로 나눈 나머지가 1 또는 3 인 경우 )
+//		n 이 5의 배수 + 2 또는 5의 배수 + 4 인 경우 ( 5로 나눈 나머지가 2 또는 4 인 경우 )
+		
+		if (N == 4 || N == 7) {
+			System.out.println(-1);
+		}
+		else if (N % 5 == 0) {
+			System.out.println(N / 5);
+		}
+		else if (N % 5 == 1 || N % 5 == 3) {
+			System.out.println((N / 5) + 1);
+		}
+		else if (N % 5 == 2 || N % 5 == 4) {
+			System.out.println((N / 5) + 2);
+		}
+		
 	}
 
 }
