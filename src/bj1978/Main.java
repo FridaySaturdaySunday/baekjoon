011941d9ee4be3f5fd25e381ba169ed6a573432b
cks <cksun2598@gmail.com>
2022-10-20 21:03:35 +0900

    d

---
+++ src/bj1978/Main.java
@@ -0,0 +1,17 @@
+package bj1978;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+	}
+
+}
