00f34a367c7f0b32151efdf258a3865de55bddb2
sun2598 <cksun2598@gmail.com>
2022-10-30 19:11:28 +0900

    d

--- src/bj2587/Main.java
+++ src/bj2587/Main.java
@@ -1,10 +1,27 @@
 package bj2587;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws NumberFormatException, IOException {
 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int[] num = new int[5];
+		
+		for(int i=0; i<5; i++) {
+			num[i] = Integer.parseInt(br.readLine());
+		}
+		
+		Arrays.sort(num);
+		
+		System.out.println(Arrays.stream(num).sum()/5);
+		System.out.println(num[2]);
+		
 	}
 
 }
