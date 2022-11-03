77049245860b6bf5cbb273dc6deaa137b4164cf7
sun2598 <cksun2598@gmail.com>
2022-11-03 23:09:11 +0900

    h

--- src/bj10989/Main.java
+++ src/bj10989/Main.java
@@ -1,10 +1,33 @@
 package bj10989;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws NumberFormatException, IOException {
 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		int[] count = new int[10001];
+		
+		for(int i=0; i<n; i++) {
+			count[Integer.parseInt(br.readLine())]++;
+		}
+		
+		for(int i=1; i<10001; i++) {
+			while(count[i] > 0) {
+				sb.append(i).append('\n');
+				count[i]--;
+			}
+		}
+		System.out.println(sb);
+		br.close();
 	}
 
 }
